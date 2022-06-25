4c380a900878f145f74006bbc0a40984bda381f1
cunzaizhuyi <877824709@qq.com>
2022-06-25 15:40:18 +0800

    feat: hint

--- deploy.sh
+++ deploy.sh
@@ -19,6 +19,7 @@ git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+# just a example, please change the follow line with your github account!!!
  git push -f https://github.com/cunzaizhuyi/cunzaizhuyi.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
